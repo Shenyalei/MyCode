23f5a9b5955c69b05591a122fabf7814f90f1b69
Summer <529854057@qq.com>
2017-03-08 10:26:43 +0800

    add HRTimer

---
+++ HRTimer.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "HRTimer.h"
+LONG64 HRtimer::GetFrequency()
+{
+	LARGE_INTEGER proc_freq;
+	::QueryPerformanceFrequency(&proc_freq);
+	return proc_freq.QuadPart;
+}
+void HRtimer::Start()
+{
+	::QueryPerformanceCounter(&start);
+}
+void HRtimer::Stop()
+{
+	::QueryPerformanceCounter(&stop);
+	LONGLONG timeDiff = stop.QuadPart - start.QuadPart;
+	LONGLONG timeDiff2 = timeDiff * 1000 * 1000 / frequency;
+	printf("HRtimer time %lld\n", timeDiff2);
+}
+LONG64 HRtimer::frequency = HRtimer::GetFrequency();
